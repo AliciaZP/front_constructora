1a4910e1313e98b440ffa7d11d24939bda73e0af
Soraya Ramos <soraya.210836@gmail.com>
2023-12-04 15:57:21 +0100

    Creadas las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ObrasComponent } from './pages/obras/obras.component';
+import { EditObraComponent } from './pages/edit-obra/edit-obra.component';
+import { OperariosComponent } from './pages/operarios/operarios.component';
+import { EditOperarioComponent } from './pages/edit-operario/edit-operario.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { NuevaObraComponent } from './pages/nueva-obra/nueva-obra.component';
+import { NuevoOperarioComponent } from './pages/nuevo-operario/nuevo-operario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/login' },
+  { path: 'home', component: HomeComponent },
+  { path: 'obras', component: ObrasComponent },
+  { path: 'obras/nuevo/', component: NuevaObraComponent },
+  { path: 'obras/edit/:obraId', component: EditObraComponent },
+  { path: 'operarios', component: OperariosComponent },
+  { path: 'operarios/nuevo/', component: NuevoOperarioComponent },
+  { path: 'operarios/edit/:operarioId', component: EditOperarioComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'error', component: ErrorComponent },
+  { path: '**', redirectTo: '/login' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { ObrasComponent } from './pages/obras/obras.component';
+import { OperariosComponent } from './pages/operarios/operarios.component';
+import { NuevaObraComponent } from './pages/nueva-obra/nueva-obra.component';
+import { EditObraComponent } from './pages/edit-obra/edit-obra.component';
+import { NuevoOperarioComponent } from './pages/nuevo-operario/nuevo-operario.component';
+import { EditOperarioComponent } from './pages/edit-operario/edit-operario.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    NavBarComponent,
+    FooterComponent,
+    ErrorComponent,
+    ObrasComponent,
+    OperariosComponent,
+    NuevaObraComponent,
+    EditObraComponent,
+    NuevoOperarioComponent,
+    EditOperarioComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
