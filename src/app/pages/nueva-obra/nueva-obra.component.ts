61c7cd24a2fd75d6128f8f025a7762ee254b098e
Soraya Ramos <soraya.210836@gmail.com>
2023-12-04 15:54:14 +0100

    Creado componente de nueva obra

---
+++ src/app/pages/nueva-obra/nueva-obra.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nueva-obra',
+  templateUrl: './nueva-obra.component.html',
+  styleUrls: ['./nueva-obra.component.css']
+})
+export class NuevaObraComponent {
+
+}
