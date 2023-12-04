145be6af7c1e5b869b83151352e4417c12aa9276
Soraya Ramos <soraya.210836@gmail.com>
2023-12-04 15:55:13 +0100

    Creado componente de edit operario

---
+++ src/app/pages/edit-operario/edit-operario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-operario',
+  templateUrl: './edit-operario.component.html',
+  styleUrls: ['./edit-operario.component.css']
+})
+export class EditOperarioComponent {
+
+}
