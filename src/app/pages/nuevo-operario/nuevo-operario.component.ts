d1be95f8cbcbcf28199b89d89e7aaec52cf05e13
Soraya Ramos <soraya.210836@gmail.com>
2023-12-04 15:54:00 +0100

    Creado componente de nuevo operario

---
+++ src/app/pages/nuevo-operario/nuevo-operario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevo-operario',
+  templateUrl: './nuevo-operario.component.html',
+  styleUrls: ['./nuevo-operario.component.css']
+})
+export class NuevoOperarioComponent {
+
+}
