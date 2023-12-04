4bc68283803aa66567f7e0ab2df0e9d4bbbd607b
Soraya Ramos <soraya.210836@gmail.com>
2023-12-04 15:55:31 +0100

    Creado componente de edit obra

---
+++ src/app/pages/edit-obra/edit-obra.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-obra',
+  templateUrl: './edit-obra.component.html',
+  styleUrls: ['./edit-obra.component.css']
+})
+export class EditObraComponent {
+
+}
