cb8811e098f807a4ee467c4547bc48262293df6e
Alicia Zamorano <azpans@hotmail.com>
2023-12-05 22:42:29 +0100

    new worker: en proceso

--- src/app/modules/workers/pages/new-worker/new-worker.component.ts
+++ src/app/modules/workers/pages/new-worker/new-worker.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { WorkersService } from 'src/app/core/services/workers.service';
 
 @Component({
   selector: 'new-worker',
@@ -6,5 +9,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-worker.component.css']
 })
 export class NewWorkerComponent {
+
+  newWorker: FormGroup;
+  workersService = inject(WorkersService)
+
+  router = inject(Router)
+
+  constructor() {
+    this.newWorker = new FormGroup({
+      name: new FormControl(null, [Validators.required, Validators.minLength(3),
+      Validators.maxLength(45)]),
+      description: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      direction: new FormControl(null, [Validators.required, Validators.minLength(3),
+      Validators.maxLength(150)]),
+      city: new FormControl(null, [Validators.required, Validators.minLength(3),
+      Validators.maxLength(70)]),
+      assignment_date: new FormControl(null, Validators.required),
+      deadline: new FormControl(null, Validators.required),
+      phone: new FormControl(null, [Validators.required, Validators.minLength(3),
+      Validators.maxLength(12)]),
+      construction_type: new FormControl(null, [Validators.required, Validators.minLength(3),
+      Validators.maxLength(45)]),
+      work_time: new FormControl(null, [Validators.required, Validators.minLength(3),
+      Validators.maxLength(150)]),
+      image: new FormControl(null, Validators.required),
+    })
+  }
+
+  onSubmit() {
+    if (this.newWorker.valid) {
+      this.workersService.createWorker(this.newWorker.value);
+      this.router.navigate(['/workers']);
+    } else {
+      console.log('error');
+    }
+  };
+
+
+  checkError(controlName: string, errorName: string) {
+    return this.newWorker.get(controlName)?.hasError(errorName) && this.newWorker.get(controlName)?.touched;
+  };
 
 }
