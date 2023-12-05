ee97c43b535294e4ff87dd2f19917610d95c3f83
Alicia Zamorano <azpans@hotmail.com>
2023-12-05 17:14:32 +0100

    componente constructions: funcionando

--- src/app/db/constructions.db.ts
+++ src/app/db/constructions.db.ts
@@ -2,14 +2,26 @@
 export const ConstructionsDB = [
     {
         _id: "construction_1",
-        name: "Madrid",
-        description: "Esto es la obra de un parque",
-        direction: "Calle Velazquez",
-        city: "Madrid",
+        name: "Madrid Nuevo Norte",
+        description: "Madrid Nuevo Norte es la marca actual del proyecto promovido por Adif  a través de la mercantil Crea Madrid Nuevo Norte, antes conocida como DUCH y DCN . En ella participan BBVA, la constructora San José y Merlin Properties para la remodelación de una parte importante del norte de Madrid, amparada en la iniciativa de los últimos gobiernos municipales de remodelar el plan parcial que afectaba a la zona (Revisión del plan parcial de reforma interior del área de planeamiento remitido 08.03 «Prolongación de la Castellana»). La actuación afecta a unas 311 hectáreas (en 5,6 km de longitud paralela a las vías del tren), y supone la creación de 3.3 millones de metros cuadrados al norte del distrito de Chamartín",
+        direction: "Calle Agustín de Foxa,s/n 28036 - Madrid",
+        city: "Comunidad de Madrid",
         assignment_date: new Date(2023, 12, 1),
         deadline: new Date(2023, 12, 31),
-        phone: "666555444",
-        construction_type: "parque",
-        work_time: "",
+        phone: "912 320 320",
+        construction_type: "Zona residencial",
+        work_time: "L-V De 08:00 a 20:00",
+    },
+    {
+        _id: "construction_2",
+        name: "Residencia de Mayores",
+        description: "Este proyecto se ha realizado en una residencia para personas mayores que se encuentra en Pobra do Caramiñal. La obra ha consistido en una rehabilitación energética del edificio.Esta edificación se encontraba en un estado de conservación bastante bueno. No se observaban problemas que pudieran afectar a la habitabilidad y seguridad de los ocupantes.El principal déficit de esta edificación era su pobre aislamiento térmico. Un gran porcentaje de la superficie era acristalada, y las carpinterías exteriores no se encontraban todas en buen estado.En esta obra, el encargo consistió en realizar una mejora de la eficiencia energética del edificio y de las condiciones de confort interior de los usuarios del centro.La principal finalidad del proyecto, que es la mejora del confort térmico interior y la eficiencia energética del mismo, queda cubierta después de aumentar el aislamiento.",
+        direction: "Rúa Venecia, 0, 15940 A Pobra do Caramiñal, A Coruña",
+        city: "Coruña",
+        assignment_date: new Date(2023, 12, 1),
+        deadline: new Date(2023, 12, 31),
+        phone: "881 86 62 70",
+        construction_type: "Edificio",
+        work_time: "S-D De 08:00 a 20:00",
     }
 ];
--- src/app/modules/constructions/pages/constructions/constructions.component.ts
+++ src/app/modules/constructions/pages/constructions/constructions.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Construction } from 'src/app/core/interfaces/construction.interfaces';
+import { ConstructionsService } from 'src/app/core/services/constructions.service';
 
 @Component({
   selector: 'constructions',
@@ -7,4 +9,62 @@ import { Component } from '@angular/core';
 })
 export class ConstructionsComponent {
 
+
+  constructionsService = inject(ConstructionsService)
+
+  arrConstructions: Construction[] = []
+
+  /*   arrCategorias: string[] = []
+    arrAutores: string[] = [] */
+
+  ngOnInit() {
+    this.arrConstructions = this.constructionsService.getAll();
+    /*     this.arrCategorias = this.postsServices.getCategorias();
+        this.arrAutores = this.postsServices.getAutores(); */
+  }
+
+
+  /*   onConstructionDelete($event: string) {
+      const response = this.constructionsService.deleteConstructionById($event)
+      this.arrConstructions = this.constructionsService.getAll();
+    }
+   */
+
+  //Aqui empiezan los fitros
+  /* 
+    onChangeCategoria($event: any) {
+      this.arrPosts = $event.target.value === "" ? this.postsServices.getAll() : this.postsServices.getByCategoria($event.target.value);
+    };
+  
+    onChangeAutor($event: any) {
+      this.arrPosts = $event.target.value === "" ? this.postsServices.getAll() : this.postsServices.getByAutor($event.target.value);
+    }
+  
+    onChangeTitulo($event: any) {
+      const ascendente = $event.target.value === "A-Z";
+      this.arrPosts = this.postsServices.orderByTitulo(ascendente);
+      //si el value no corresponde, la funcion ejectua en orden descendente
+    }
+  
+    onChangeFecha($event: any) {
+      const ascendente = $event.target.value === "reciente";
+      this.arrPosts = this.postsServices.orderByDate(ascendente);
+    } //si el value no corresponde, la funcion ejectua en orden descendente
+  
+   */
+
 }
+
+
+/* 
+getCities() { }
+filterByCity() { }
+
+getConstructionTypes() { }
+filterByConstructionType() { }
+
+orderByName() { }
+
+orderByAssignmentDate() { }
+
+orderByDeadline() { } */
