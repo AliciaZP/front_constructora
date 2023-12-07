d38bf8d0994b16aebd65939c06cf5e0628ea95cf
Alicia Zamorano <azpans@hotmail.com>
2023-12-07 19:02:34 +0100

    Update base de datos

--- src/app/db/constructions.db.ts
+++ src/app/db/constructions.db.ts
@@ -2,7 +2,7 @@
 export const ConstructionsDB = [
     {
         _id: "construction_1",
-        name: "Madrid Nuevo Norte",
+        name: "Nombre Obra",
         description: "Madrid Nuevo Norte es la marca actual del proyecto promovido por Adif  a través de la mercantil Crea Madrid Nuevo Norte, antes conocida como DUCH y DCN . En ella participan BBVA, la constructora San José y Merlin Properties para la remodelación de una parte importante del norte de Madrid, amparada en la iniciativa de los últimos gobiernos municipales de remodelar el plan parcial que afectaba a la zona (Revisión del plan parcial de reforma interior del área de planeamiento remitido 08.03 «Prolongación de la Castellana»). La actuación afecta a unas 311 hectáreas (en 5,6 km de longitud paralela a las vías del tren), y supone la creación de 3.3 millones de metros cuadrados al norte del distrito de Chamartín",
         direction: "Calle Agustín de Foxa, s/n 28036 - Madrid",
         city: "Madrid",
@@ -11,11 +11,11 @@ export const ConstructionsDB = [
         phone: "912 320 320",
         construction_type: "Urbanización",
         work_time: "L-V De 08:00 a 20:00",
-        image: "https://creamadridnuevonorte.com/web/app/uploads/2021/11/MNN-skyline.jpg"
+        image: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Palacio_Real_de_Madrid_Julio_2016_%28cropped%29.jpg"
     },
     {
         _id: "construction_2",
-        name: "Residencia de Mayores",
+        name: "Nombre Obra",
         description: "Este proyecto se ha realizado en una residencia para personas mayores que se encuentra en Pobra do Caramiñal. La obra ha consistido en una rehabilitación energética del edificio.Esta edificación se encontraba en un estado de conservación bastante bueno. No se observaban problemas que pudieran afectar a la habitabilidad y seguridad de los ocupantes.El principal déficit de esta edificación era su pobre aislamiento térmico. Un gran porcentaje de la superficie era acristalada, y las carpinterías exteriores no se encontraban todas en buen estado.En esta obra, el encargo consistió en realizar una mejora de la eficiencia energética del edificio y de las condiciones de confort interior de los usuarios del centro.La principal finalidad del proyecto, que es la mejora del confort térmico interior y la eficiencia energética del mismo, queda cubierta después de aumentar el aislamiento.",
         direction: "Rúa Venecia, 0, 15940 A Pobra do Caramiñal, A Coruña",
         city: "Coruña",
@@ -24,10 +24,10 @@ export const ConstructionsDB = [
         phone: "881 86 62 70",
         construction_type: "Edificio",
         work_time: "S-D De 08:00 a 20:00",
-        image: "https://www.paxinasgalegas.es/imagenes/pobra-do-carami%C3%B1al_img120917t0m0w1600h800.jpg"
+        image: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Palacio_Real_de_Madrid_Julio_2016_%28cropped%29.jpg"
     }, {
         _id: "construction_3",
-        name: "Sagrada Familia",
+        name: "Nombre Obra",
         description: "La Sagrada Familia es un templo expiatorio diseñado por el arquitecto Antoni Gaudí, situado en Barcelona. Es uno de los íconos más reconocibles de España. La construcción comenzó en 1882 y aún está en curso. La Sagrada Familia combina varios estilos arquitectónicos y presenta una rica simbología religiosa en su diseño.",
         direction: "Carrer de Mallorca, 401, 08013 Barcelona",
         city: "Barcelona",
@@ -36,12 +36,12 @@ export const ConstructionsDB = [
         phone: "932 08 04 14",
         construction_type: "Monumento",
         work_time: "L-D De 09:00 a 18:00",
-        image: "https://images.theconversation.com/files/439680/original/file-20220106-27-cwqd9.jpg?ixlib=rb-1.1.0&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip"
+        image: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Palacio_Real_de_Madrid_Julio_2016_%28cropped%29.jpg"
     },
 
     {
         _id: "construction_4",
-        name: "Alhambra",
+        name: "Nombre Obra",
         description: "La Alhambra es un palacio y fortaleza situado en Granada, Andalucía. Es conocida por su arquitectura islámica, sus exquisitos jardines y sus detalles artísticos. Construida principalmente durante el siglo XIII, la Alhambra es un importante patrimonio cultural y turístico de España.",
         direction: "Calle Real de la Alhambra, s/n, 18009 Granada",
         city: "Granada",
@@ -50,12 +50,12 @@ export const ConstructionsDB = [
         phone: "958 02 79 71",
         construction_type: "Monumento",
         work_time: "L-D De 08:30 a 18:00",
-        image: "https://upload.wikimedia.org/wikipedia/commons/d/de/Dawn_Charles_V_Palace_Alhambra_Granada_Andalusia_Spain.jpg"
+        image: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Palacio_Real_de_Madrid_Julio_2016_%28cropped%29.jpg"
     },
 
     {
         _id: "construction_5",
-        name: "Parque Güell",
+        name: "Nombre Obra",
         description: "El Parque Güell es un parque público diseñado por Antoni Gaudí en Barcelona. Es conocido por sus coloridos mosaicos y esculturas, y ofrece vistas panorámicas de la ciudad. El parque fue inaugurado en 1926 y es otro ejemplo destacado del modernismo catalán.",
         direction: "Carrer d'Olot, s/n, 08024 Barcelona",
         city: "Barcelona",
@@ -64,10 +64,10 @@ export const ConstructionsDB = [
         phone: "932 13 71 00",
         construction_type: "Parque",
         work_time: "L-D De 08:00 a 20:00",
-        image: "https://images.musement.com/cover/0002/15/park-guell_header-114423.jpeg"
+        image: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Palacio_Real_de_Madrid_Julio_2016_%28cropped%29.jpg"
     }, {
         _id: "construction_5",
-        name: "Catedral de Santiago de Compostela",
+        name: "Nombre Obra",
         description: "La Catedral de Santiago de Compostela es un importante lugar de peregrinación en España. Construida en el siglo IX, la catedral es famosa por albergar las reliquias del apóstol Santiago. Su arquitectura románica y gótica la convierte en una joya histórica y religiosa.",
         direction: "Plaza del Obradoiro, s/n, 15704 Santiago de Compostela, A Coruña",
         city: "Coruña",
@@ -76,11 +76,11 @@ export const ConstructionsDB = [
         phone: "981 58 31 88",
         construction_type: "Monumento",
         work_time: "L-D De 07:00 a 20:00",
-        image: "https://www.caminandocontigo.com/wp-content/uploads/2020/09/CATEDRAL-SANTIAGO-HISTORIA.jpg"
+        image: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Palacio_Real_de_Madrid_Julio_2016_%28cropped%29.jpg"
     },
     {
         _id: "construction_7",
-        name: "Palacio Real de Madrid",
+        name: "Nombre Obra",
         description: "El Palacio Real de Madrid es la residencia oficial de la monarquía española. Con una arquitectura impresionante, el palacio cuenta con más de 3,000 habitaciones y es utilizado para ceremonias oficiales. Los jardines y salones históricos hacen del palacio una atracción turística única.",
         direction: "Calle de Bailén, s/n, 28071 Madrid",
         city: "Madrid",
@@ -94,7 +94,7 @@ export const ConstructionsDB = [
 
     {
         _id: "construction_8",
-        name: "Ciudad de las Artes y las Ciencias",
+        name: "Nombre Obra",
         description: "La Ciudad de las Artes y las Ciencias es un complejo arquitectónico, cultural y de entretenimiento en Valencia. Diseñado por Santiago Calatrava y Félix Candela, el complejo incluye un planetario, un museo de ciencias y un impresionante auditorio. Es un referente de la arquitectura contemporánea.",
         direction: "Av. del Professor López Piñero, 7, 46013 València",
         city: "Valencia",
@@ -103,7 +103,7 @@ export const ConstructionsDB = [
         phone: "961 97 59 00",
         construction_type: "Parque",
         work_time: "L-D De 10:00 a 21:00",
-        image: "https://cd1.taquilla.com/data/images/t/83/ciudad-de-las-artes-y-las-ciencias.jpg"
+        image: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Palacio_Real_de_Madrid_Julio_2016_%28cropped%29.jpg"
     },
 
 
