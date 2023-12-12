3ee58777c7687815f4ee8020997b34b70687fcb9
Soraya Ramos <soraya.210836@gmail.com>
2023-12-12 10:50:00 +0100

    Componente Card

--- src/app/shared/components/card-workers/card-workers.component.ts
+++ src/app/shared/components/card-workers/card-workers.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from 'src/app/core/interfaces/user.interface';
 
 @Component({
   selector: 'app-card-workers',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class CardWorkersComponent {
 
+  @Input() workers: User;
+
+  constructor() {
+    this.workers = {
+      _id: '',
+      name: 'Soraya',
+      surname: 'Ramos',
+      dni: '',
+      phone: '',
+      email: '',
+      password: '',
+      role: 'Administrador',
+      active: false,
+      job: '',
+      city: '',
+      image: 'https://media.istockphoto.com/id/1331101143/photo/smiling-confident-mature-businessman-looking-at-camera-standing-in-office.jpg?s=612x612&w=0&k=20&c=M6Xv9Zt1RYkKkf0XVuIKpNvhnHgXPX_WWCemPdChfTE='
+    }
+  }
 }
