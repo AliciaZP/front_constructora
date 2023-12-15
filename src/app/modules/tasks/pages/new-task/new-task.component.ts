6ad55187cb0b191e081b1da0d940fd0b0c685723
Alicia Zamorano <azpans@hotmail.com>
2023-12-15 14:26:45 +0100

    Sweet Alert: reports funcionando

--- src/app/modules/tasks/pages/new-task/new-task.component.ts
+++ src/app/modules/tasks/pages/new-task/new-task.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TasksService } from 'src/app/core/services/tasks.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-new-task',
@@ -27,15 +28,32 @@ export class NewTaskComponent {
     })
   }
 
-  onSubmit() {
+  async onSubmit() {
     if (this.newTask.valid) {
-      this.tasksService.createTask(this.newTask.value);
-      this.router.navigate(['/tasks']);
+      await this.tasksService.createTask(this.newTask.value);
+      Swal.fire({
+        icon: 'success',
+        title: 'Tarea creada correctamente',
+        confirmButtonText: 'Aceptar',
+        confirmButtonColor: '#008000',
+        color: 'white',
+        background: '#0f0f0f',
+      }).then(() => {
+        this.router.navigate(['/tasks']);
+      });
     } else {
       console.log('error');
+      Swal.fire({
+        icon: 'error',
+        title: 'Datos erróneos',
+        text: 'Por favor, completa todos los campos de la tarea de forma correcta.',
+        confirmButtonText: 'Aceptar',
+        confirmButtonColor: '#af1e2d',
+        color: 'white',
+        background: '#0f0f0f',
+      });
     }
-  };
-
+  }
 
   checkError(controlName: string, errorName: string) {
     return this.newTask.get(controlName)?.hasError(errorName) && this.newTask.get(controlName)?.touched;
