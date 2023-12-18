7ea739901ad75468c7d4e63b4ef951efb2797bc8
Alicia Zamorano <azpans@hotmail.com>
2023-12-19 00:59:28 +0100

    Map: funcionando el map de la home

--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ConstructionsService } from 'src/app/core/services/constructions.service';
+
 
 @Component({
   selector: 'app-map',
@@ -8,53 +10,47 @@ import { Component } from '@angular/core';
 export class MapComponent {
 
   center: google.maps.LatLngLiteral;
-  zoom: number = 17;
+  zoom: number = 6; //max 22
   mapTypeId: google.maps.MapTypeId = google.maps.MapTypeId.HYBRID;
-  myPosition!: google.maps.LatLngLiteral;
-  myMarkerOptions: google.maps.MarkerOptions;
-  arrPositions: google.maps.LatLng[] = [];
+  constructions: any[] = []
+  constructionsService = inject(ConstructionsService)
+
+  async ngOnInit() {
+    this.constructions = await this.constructionsService.getAllConstructions()
+  }
 
   constructor() {
     this.center = {
-      lat: 39.452887,
-      lng: -0.360415,
-    };
-    this.myMarkerOptions = {
-      /* 
-            animation: google.maps.Animation.BOUNCE, // Animación del marcador (puedes usar DROP en lugar de BOUNCE)
-            draggable: true, // Permite arrastrar el marcador
-            icon: 'path/to/custom/icon.png', // Ruta a una imagen personalizada para el icono del marcador
-            label: 'A', // Etiqueta del marcador (puede ser un carácter o una cadena)
-            title: 'Mi Marcador', // Título del marcador que se muestra al pasar el ratón sobre él
-            opacity: 0.7, // Opacidad del marcador (valor entre 0 y 1)
-            visible: true, // Indica si el marcador es visible
-            zIndex: 100, // Índice de apilamiento del marcador
-            // Más opciones específicas del marcador: https://developers.google.com/maps/documentation/javascript/markers#marker_options
-         */
+      lat: 40,
+      lng: -3,
     }
   }
 
-  ngAfterViewInit() {
-    navigator.geolocation.getCurrentPosition(position => {
-      console.log(position);
-      this.center = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      }
-      this.myPosition = { ...this.center }
-    })
-  }
 
-  onMapClick($event: google.maps.MapMouseEvent) {
-    if ($event.latLng) {
-      this.arrPositions.push($event.latLng);
+  onChangeType($event: any) {
+    //si google cambia el nombre de los tipos de mapa esto deja de funcionar
+    // this.mapTypeId = $event.target.value
+    switch ($event.target.value) {
+      case 'hybrid':
+        this.mapTypeId = google.maps.MapTypeId.HYBRID;
+        break;
+      case 'terrain':
+        this.mapTypeId = google.maps.MapTypeId.TERRAIN;
+        break;
+      case 'roadmap':
+        this.mapTypeId = google.maps.MapTypeId.ROADMAP;
+        break;
+      case 'satellite':
+        this.mapTypeId = google.maps.MapTypeId.SATELLITE;
+        break;
     }
   }
 
-  onMarkerClick($event: google.maps.MapMouseEvent, index: number) {
-    this.arrPositions.splice(index, 1)
+  generateIcon(construction: any) {
+    return {
+      url: "https://cdn-icons-png.flaticon.com/512/2810/2810796.png",
+      scaledSize: new google.maps.Size(30, 30),
+    }
   }
-
 
 }
-
